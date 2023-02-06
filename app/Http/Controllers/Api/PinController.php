deda5b1f8655d41834122155bf568133b6f5bbb9
Berkay Çatak <34205493+berkaycatak@users.noreply.github.com>
2023-02-06 13:37:46 +0300

    api

---
+++ app/Http/Controllers/Api/PinController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pin;
+use Illuminate\Http\Request;
+
+class PinController extends Controller
+{
+    public function list(Request $request){
+        $pins = Pin::get();
+        return $pins;
+    }
+
+    public function show(Request $request){
+        $pin = Pin::find($request->id);
+        return $pin;
+    }
+
+    public function store(Request $request){
+        $pin = new Pin();
+        $pin->lat = $request->lat;
+        $pin->lng = $request->lng;
+        $pin->description = $request->description;
+        $pin->name_surname = $request->name_surname;
+        $pin->phone_number = $request->phone_number;
+        $pin->who = $request->who;
+        $check = $pin->save();
+
+        if ($check)
+            return $pin;
+        else
+            return json_encode(["error" => 1, "message" => "Pin eklenirken hata oluştu"]);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\PinController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/pins', [PinController::class, 'list']);
+Route::post('/pins/add', [PinController::class, 'store']);
+//Route::post('/pins/{id}', [PinController::class, 'show']);
+
